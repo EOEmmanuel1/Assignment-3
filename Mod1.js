a860563e8fe72e0b0e8eeebf9cfed7a819c6f17f
EOEmmanuel1 <166291941+EOEmmanuel1@users.noreply.github.com>
2024-04-26 12:53:24 -0700

    Add files via upload
    week3 Assignment 1 and 2

---
+++ Mod1.js
@@ -0,0 +1,16 @@
+// h1 addEventListener("click",{})=>{
+//     h1 Style.color= "red"
+// }
+
+
+// const button = document.getElementById("tt")
+
+// const runSomething =()=>{}
+// button.addEventListener("click", runSomething)
+// h1.color="yellow"
+
+document.getElementById("welcome").style.color = "green"
+
+const topic = getElementById("assignment")
+const button = getElementById("btn")
+button addEventListener
